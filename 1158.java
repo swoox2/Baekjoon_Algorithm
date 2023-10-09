c0564599f52a7c6f0c0fc4385fb4aa7e4c9f6b12
Sang Woo <80804697+swoox2@users.noreply.github.com>
2023-10-09 23:09:28 +0900

    Create 1158.java

---
+++ 1158.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+
+        for(int i=1; i<=n; i++) q.add(i);
+
+        while (!q.isEmpty()) {
+
+            for (int i = 0; i < k - 1; i++) {
+                int x=q.poll();
+                q.offer(x);
+            }
+            arr.add(q.poll());
+        }
+
+        bw.write("<");
+        for(int i=0; i<arr.size(); i++){
+            if(i!=arr.size()-1) bw.write(arr.get(i)+", ");
+            else bw.write(String.valueOf(arr.get(i)));
+        }
+        bw.write(">");
+
+        br.close();
+        bw.close();
+    }
+
+}
