0084593cd9ae48a5f62e61c65f1103f9c841053c
Sang Woo <wsw960307@gmail.com>
2024-01-21 20:07:11 +0900

    Create 2743.java

---
+++ 2743.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        String s=br.readLine();
+
+        System.out.println(s.length());
+
+        br.close();
+    }
+}
