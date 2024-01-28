38976a8a42687340d91d5ced592e397175e3c607
Sang Woo <wsw960307@gmail.com>
2024-01-28 19:10:35 +0900

    Create 25305.java

---
+++ 25305.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st=new StringTokenizer(br.readLine());
+
+        int n=Integer.parseInt(st.nextToken());
+        int k=Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> list=new ArrayList<>();
+
+        st=new StringTokenizer(br.readLine());
+
+        for(int i=0; i<n; i++) list.add(Integer.parseInt(st.nextToken()));
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        System.out.println(list.get(k-1));
+
+        br.close();
+    }
+}
