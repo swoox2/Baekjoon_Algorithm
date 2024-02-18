8cc7a6b8279577314a23092dfae9d2d6b90e6683
Sang Woo <wsw960307@gmail.com>
2024-02-18 22:04:40 +0900

    Create 10817.java

---
+++ 10817.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st=new StringTokenizer(br.readLine());
+
+        int[] arr=new int[3];
+
+        for(int i=0; i<3; i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[1]);
+
+        br.close();
+    }
+}
